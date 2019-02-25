7396719095d3e8d0a68cebf0ff46e6beee5e1d32
Dmitry <sellect.47@gmail.com>
2019-02-26 00:36:22 +0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import controller.MenuController;
 import utils.MyBatisUtils;
 
+//точка входа
 public class Main {
 	public static void main(String[] args) {
-		MyBatisUtils.initSqlSessionFactory();
-		new MenuController();
+		MyBatisUtils.initSqlSessionFactory();	// настройка подключения к бд
+		new MenuController();	
 	}
 }
